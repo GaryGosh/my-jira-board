4cb1236ebd67f4b2d4f1e75c805df5cceb2a4802
Neeraj TK <64883030+GaryGosh@users.noreply.github.com>
2023-10-24 20:48:04 +0530

    bug fix

--- src/components/DarkModeIconButton.tsx
+++ src/components/DarkModeIconButton.tsx
@@ -1,20 +1,20 @@
-import { MoonIcon, SunIcon } from '@chakra-ui/icons';
-import { IconButton, useColorMode } from '@chakra-ui/react';
-import React from 'react';
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { IconButton, useColorMode } from "@chakra-ui/react";
+import React from "react";
 
 function DarkModeIconButton({
   ...rest
 }: React.ComponentPropsWithoutRef<typeof IconButton>) {
   const { colorMode, toggleColorMode } = useColorMode();
 
-  const isDark = colorMode === 'dark';
+  const isDark = colorMode === "dark";
 
   return (
     <IconButton
       onClick={toggleColorMode}
       icon={isDark ? <MoonIcon /> : <SunIcon />}
-      aria-label={'dark-mode-toggle'}
       {...rest}
+      aria-label={"dark-mode-toggle"}
     />
   );
 }
